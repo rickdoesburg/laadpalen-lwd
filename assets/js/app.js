397229cb35fcefbc4e43d05e754219a1b3c50701
Rick Doesburg <rickdoesburg@gmail.com>
2020-01-29 22:49:07 +0100

    Removed old index file, turned off stubs

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,35 +1,35 @@
-// const APIUrl = "https://oplaadpalen.nl/api/maplist/location/04beae18-d5f6-11e9-a9fa-42010a840003";
+const APIUrl = "https://oplaadpalen.nl/api/maplist/location/04beae18-d5f6-11e9-a9fa-42010a840003";
 
 const vm = new Vue({
     el: '#app',
     data: {
-        // stub
-        results: [
-            {ocpi_evse_id: "NL*NUO*EEVB*P1531033*2", status: "AVAILABLE"},
-            {ocpi_evse_id: "NL*NUO*EEVB*P1531033*1", status: "CHARGING"}
-        ]
-        // results: []
+        // // stub
+        // results: [
+        //     {ocpi_evse_id: "NL*NUO*EEVB*P1531033*2", status: "AVAILABLE"},
+        //     {ocpi_evse_id: "NL*NUO*EEVB*P1531033*1", status: "CHARGING"}
+        // ]
+        results: []
     },
     mounted() {
-        // this.getStatus(status);
+        this.getStatus(status);
 
-        // setInterval(function () {
-        //   this.getStatus();
-        //   console.log('Polling');
-        // }.bind(this), 60000);
+        setInterval(function () {
+          this.getStatus();
+          console.log('Polling');
+        }.bind(this), 60000);
     },
     methods: {
 
-        // getStatus() {
-        //     axios.get(APIUrl).then((response) => {
-        //         // Dive in the JSON
-        //         let chargingSpots = response.data.data.chargingstation.chargingspots
+        getStatus() {
+            axios.get(APIUrl).then((response) => {
+                // Dive in the JSON
+                let chargingSpots = response.data.data.chargingstation.chargingspots
 
-        //         this.results = chargingSpots
+                this.results = chargingSpots
 
-        //     }).catch(error => { console.log(error); });
+            }).catch(error => { console.log(error); });
 
-        // },
+        },
 
          isAvailable(chargingspot) {
             return chargingspot.status == 'AVAILABLE'
