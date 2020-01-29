e18ceb85d4fb22281cddb9f75ed451e76a87cae0
Rick Doesburg <rickdoesburg@gmail.com>
2020-01-29 22:35:03 +0100

    Styling, added stubdata, added polling

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,31 +1,38 @@
-const APIUrl = "https://oplaadpalen.nl/api/maplist/location/04beae18-d5f6-11e9-a9fa-42010a840003";
+// const APIUrl = "https://oplaadpalen.nl/api/maplist/location/04beae18-d5f6-11e9-a9fa-42010a840003";
 
 const vm = new Vue({
     el: '#app',
     data: {
-        results: []
+        // stub
+        results: [
+            {ocpi_evse_id: "NL*NUO*EEVB*P1531033*2", status: "AVAILABLE"},
+            {ocpi_evse_id: "NL*NUO*EEVB*P1531033*1", status: "CHARGING"}
+        ]
+        // results: []
     },
     mounted() {
-           this.getStatus();
-       },
-       methods: {
+        // this.getStatus(status);
 
-           getStatus() {
-               axios.get(APIUrl).then((response) => {
-                   // Dive in the JSON
-                   let chargingSpots = response.data.data.chargingstation.chargingspots
-                   
-                   this.results = chargingSpots
+        // setInterval(function () {
+        //   this.getStatus();
+        //   console.log('Polling');
+        // }.bind(this), 60000);
+    },
+    methods: {
 
-               }).catch( error => { console.log(error); });
-           },
+        // getStatus() {
+        //     axios.get(APIUrl).then((response) => {
+        //         // Dive in the JSON
+        //         let chargingSpots = response.data.data.chargingstation.chargingspots
 
-           getClass(property){
-             return {
-               'is-available': this.results[property],
-               'is-unavailable': !this.results[property]
-             }
-           }
+        //         this.results = chargingSpots
 
+        //     }).catch(error => { console.log(error); });
+
+        // },
+
+         isAvailable(chargingspot) {
+            return chargingspot.status == 'AVAILABLE'
+        }
     }
 });
