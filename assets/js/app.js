ad02cd5eb7a49bc738973633978379d92a98337e
Rick Doesburg <rdoesburg@ilionx.com>
2020-01-30 11:35:24 +0100

    Removed Browsersync, added new API

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,10 @@
-const APIUrl = "https://oplaadpalen.nl/api/maplist/location/04beae18-d5f6-11e9-a9fa-42010a840003";
+// const APIUrl = "https://oplaadpalen.nl/api/maplist/location/04beae18-d5f6-11e9-a9fa-42010a840003";
+const APISmoovURL = "https://www.smoovapp.eu/api/feature/experienceaccelerator/areas/chargepointmap/getchargepoints/04beae18-d5f6-11e9-a9fa-42010a840003";
 
 const vm = new Vue({
     el: '#app',
     data: {
-        // // stub
+        // stub
         // results: [
         //     {ocpi_evse_id: "NL*NUO*EEVB*P1531033*2", status: "AVAILABLE"},
         //     {ocpi_evse_id: "NL*NUO*EEVB*P1531033*1", status: "CHARGING"}
@@ -21,17 +22,41 @@ const vm = new Vue({
     methods: {
 
         getStatus() {
-            axios.get(APIUrl).then((response) => {
+            axios.get(APISmoovURL).then((response) => {
                 // Dive in the JSON
-                let chargingSpots = response.data.data.chargingstation.chargingspots
+                // Save responseData to var
+                let responseData = response.data
 
-                this.results = chargingSpots
+                // If the data contains evses (and thus comes from Smoov)
+                if (responseData.evses) {
 
+                    // Create empty array
+                    var results = []
+
+                    // For each ite (element) in evses, push data to array
+                    responseData.evses.forEach(element => {
+                        var elementName = element.id.split('|');
+
+                        results.push({
+                            ocpi_evse_id: elementName[0],
+                            status: (element.status == "Occupied") ? "CHARGING" : "AVAILABLE"
+                        })
+                    })
+                    this.results = results
+                } else {
+                    let chargingSpots = response.data.data.chargingstation.chargingspots
+                    this.results = chargingSpots
+                }
             }).catch(error => { console.log(error); });
-
         },
 
-         isAvailable(chargingspot) {
+        // Set class based on availability
+        getClass(chargingspot) {
+            return (chargingspot.status === 'AVAILABLE')?'is-available':'is-unavailable'
+        },
+
+        // Check if spot is available
+        isAvailable(chargingspot) {
             return chargingspot.status == 'AVAILABLE'
         }
     }
